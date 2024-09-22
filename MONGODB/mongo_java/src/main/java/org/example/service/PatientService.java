29d500d357119e04d941eb6eb48a3e264d914962
pierre rubens milorme <prubensmilorme@gmail.com>
2024-09-21 20:34:57 -0400

    add methods in patient service

--- MONGODB/mongo_java/src/main/java/org/example/service/PatientService.java
+++ MONGODB/mongo_java/src/main/java/org/example/service/PatientService.java
@@ -2,8 +2,10 @@ package org.example.service;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
-import org.example.entity.Medecin;
+import org.bson.types.ObjectId;
 import org.example.entity.Patient;
 
 public class PatientService {
@@ -14,8 +16,48 @@ public class PatientService {
     }
 
     public void insertPatient(Patient patient) {
-        Document doc = new Document("nom", patient.getNom());
-                // .append("sexe", medecin.getSexe());
+        Document addressDoc = new Document("numero", patient.getAdresse().getNumero())
+                .append("rue", patient.getAdresse().getRue())
+                .append("code_postal", patient.getAdresse().getCodePostal())
+                .append("ville", patient.getAdresse().getVille());
+
+        Document doc = new Document("numero_securite_sociale", patient.getNumeroSecuriteSociale())
+                .append("nom", patient.getNom())
+                .append("sexe", patient.getSexe())
+                .append("date_naissance", patient.getDateNaissance())
+                .append("email", patient.getEmail())
+                .append("poids", patient.getPoids())
+                .append("hauteur", patient.getHauteur())
+                .append("list_telephones", patient.getListTelephones())
+                .append("list_prenoms", patient.getListPrenoms())
+                .append("adresse", addressDoc)
+                .append("list_rendez_vous", patient.getListRendezVous())
+                .append("list_consultations", patient.getListConsultations())
+                .append("antecedents_medicaux", patient.getAntecedentsMedicaux())
+                .append("allergies", patient.getAllergies());
+
         patientCollection.insertOne(doc);
+    }
+
+    public Patient getPatientById(String id) {
+        Document query = new Document("_id", new ObjectId(id));
+        Document result = patientCollection.find(query).first();
+        if (result != null) {
+            Patient patient = new Patient();
+            patient.setId(result.getObjectId(id));
+            patient.setNom(result.getString("nom"));
+            patient.setEmail(result.getString("email"));
+            patient.setSexe(result.getString("sexe"));
+            return patient;
+        }
+        return null;
+    }
+
+    public void updatePatient(String id, String email) {
+        patientCollection.updateOne(Filters.eq("_id", new ObjectId(id)), Updates.set("email", email));
+    }
+
+    public void deletePatient(String id) {
+        patientCollection.deleteOne(Filters.eq("_id", new ObjectId(id)));
     }
 }
